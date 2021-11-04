3ad0d858e190d22a45e8b87c9852af151dfa6a04
Duc Phan <91093389+ducttphan@users.noreply.github.com>
2021-11-03 20:46:58 -0700

    Update aracari_webapp.py

--- aracari_webapp.py
+++ aracari_webapp.py
@@ -726,7 +726,7 @@ def PBMCInfiltration() :
 #Main function 
 main_modeOptions = ['ReadMe', 'Tumor Growth Analysis', 'Vessel Morphometry Analysis', 'Vessel Permeability Analysis', 'PBMC Infiltration Analysis']
 
-st.image(image='/Users/ducphan/Desktop/Streamlit/Linear_Logo.tif')
+st.image(image='Linear_Logo.tif')
 st.write('  ')
 st.header('Select Type of Analysis:')
 mode = st.radio("", main_modeOptions, index=0)
